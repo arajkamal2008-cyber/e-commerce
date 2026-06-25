3450ece2ea2feceb506ee7f1522137b56929288b
arajkamal2008-cyber <arajkamal@2008gmail.com>
2026-06-25 18:44:47 +0530

    product

---
+++ components/homeComponents/HomeCategory.tsx
@@ -0,0 +1,25 @@
+"use client"
+import React from 'react'
+import "./HomeCategory.css";
+import { categories } from '@/data/productsData';
+const HomeCategory: React.FC = () => {
+  return (
+    <>
+    <div className="home_category_section">
+      <div className="category_header">shop by category</div>
+      
+      <div className="category_images">
+        {categories.map((category, index) => {
+            return (
+                <div key={index} className="cat_card" style={{backgroundImage: `url(${category.image})`}}>
+                    <div className= "cat_card">{category.name}</div>
+                </div>
+            )
+        })}
+      </div>
+      </div>
+      </>
+  )
+}
+
+export default HomeCategory
